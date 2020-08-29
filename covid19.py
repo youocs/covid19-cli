fa74b09ae8e0ed081b29edf6fa5651a007abe5f3
Ali Alshaikh <59560043+youocs@users.noreply.github.com>
2020-08-29 07:52:14 +0300

    Update covid19.py

--- covid19.py
+++ covid19.py
@@ -2,35 +2,43 @@
 
 import urllib.request
 import json
-import sys
+from  sys import argv
 
-if len(sys.argv) != 2 and len(sys.argv) != 3:
-	print("Commands:\n  countries	  Lists all countries\n  attributes      Lists all attributes\nUsage:\n  covid19 country attribute")
-	exit()
+#Filters out incorrect command line arguments
+if len(argv) != 2 and len(argv) != 3:
+        print("Commands:\n  countries     Lists available countries\n  attributes      Lists all attributes\nUsage:\n  covid19 country attribute")
+        exit()
+
+if len(argv) == 2 and argv[1] != "countries" and argv[1] != "attributes":
+        print("Commands:\n  countries     Lists available countries\n  attributes      Lists all attributes\nUsage:\n  covid19 country attribute")
+        exit()
 
 #Opens the covid19 API (https://covid19api.com/)
 API_URL = "https://api.covid19api.com/summary"
 with urllib.request.urlopen(API_URL) as response:
-	JSON = response.read()
-	data = json.loads(JSON)
-	
-	if sys.argv[1] == "countries":
-		print("global")
-		for i in data["Countries"]:
-			country = i["Slug"]
-			print(country)
-	elif sys.argv[1] == "attributes":
-		for i in data["Global"]:
-			print(i)
-	elif sys.argv[1].lower() == "global":
-		print(data["Global"][sys.argv[2]])
+        JSON = response.read()
+        data = json.loads(JSON)
+        
+        #Executes the commands
+        if argv[1].lower == "countries":
+                print("global")
+                for i in data["Countries"]:
+                        country = i["Slug"]
+                        print(country)
+        elif argv[1].lower == "attributes":
+                for i in data["Global"]:
+                        print(i)
+       
+        elif argv[1].lower() == "global":
+                print(data["Global"][sys.argv[2]])
+        else:
+                country = argv[1]
+                attribute = argv[2]
+                
+                #Iterates over the data until it finds the requested country's segment
+                for i in data["Countries"]:
+                        if i["Slug"].lower() == country.lower():
+                                print(attribute, ": ", i[attribute], sep="")
+                                exit()
 
-	else:	
-		country = sys.argv[1]
-		attribute = sys.argv[2]
-		
-		for i in data["Countries"]:
-			if i["Slug"].lower() == country.lower():
-				print(attribute, ": ", i[attribute], sep="")
-				exit()
-		print("Incorrect attribute or country.")
+                print("Incorrect country or attribute.")
