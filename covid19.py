730da3f9b92b38209043ae526577f1e4182607d4
Ali Alshaikh <59560043+youocs@users.noreply.github.com>
2020-08-29 07:57:30 +0300

    Update covid19.py

--- covid19.py
+++ covid19.py
@@ -18,7 +18,7 @@ API_URL = "https://api.covid19api.com/summary"
 with urllib.request.urlopen(API_URL) as response:
         JSON = response.read()
         data = json.loads(JSON)
-        
+
         #Executes the commands
         if argv[1].lower == "countries":
                 print("global")
@@ -28,13 +28,13 @@ with urllib.request.urlopen(API_URL) as response:
         elif argv[1].lower == "attributes":
                 for i in data["Global"]:
                         print(i)
-       
+
         elif argv[1].lower() == "global":
                 print(data["Global"][sys.argv[2]])
         else:
                 country = argv[1]
                 attribute = argv[2]
-                
+
                 #Iterates over the data until it finds the requested country's segment
                 for i in data["Countries"]:
                         if i["Slug"].lower() == country.lower():
